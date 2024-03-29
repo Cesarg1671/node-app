79c7537dea4f41808f3642e745649c4e931aeed2
Cesarg1671 <cgarciawtw@gmail.com>
2024-03-28 19:37:22 -0700

    Function Working

--- routes/employeeRoute.js
+++ routes/employeeRoute.js
@@ -1,7 +1,8 @@
 import express from "express"
-import {fetch} from "../controller/employeeContoller.js"
+import {create, fetch} from "../controller/employeeContoller.js"
 const route = express.Router()
 
+route.post("/create", create)
 route.get("/fetch", fetch)
 
 export default route
