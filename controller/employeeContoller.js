85a044b31331d9a7d1190d098ce83050a00b9aaa
Cesarg1671 <cgarciawtw@gmail.com>
2024-03-30 13:12:23 -0700

    update workng

--- controller/employeeContoller.js
+++ controller/employeeContoller.js
@@ -25,7 +25,26 @@ export const fetch = async(req, res) => {
         if(employees.length === 0) {
             return res.status(404).json({message: "Employees not found!"})
         }
+
+        res.status(200).json(employees)
     } catch (error) {
         res.status(500).json({error: "Internal Server Error"})
+    }
+}
+
+export const update = async(req, res) => {
+    try {
+        const id = req.params.id
+        const employeeExist = await Employee.findOne({_id:id})
+
+        if(!employeeExist) {
+            return res.status(400).json({message: "Employee not found"})
+        }
+
+        const updateEmployee = await Employee.findByIdAndUpdate(id, req.body, {new: true})
+        res.status(201).json(updateEmployee)
+    } catch (error) {
+        res.satus(500).json({error: "internal Server Error"})
+        
     }
 }
--- routes/employeeRoute.js
+++ routes/employeeRoute.js
@@ -1,8 +1,9 @@
 import express from "express"
-import {create, fetch} from "../controller/employeeContoller.js"
+import {create, fetch, update} from "../controller/employeeContoller.js"
 const route = express.Router()
 
 route.post("/create", create)
 route.get("/fetch", fetch)
+route.put("/update/:id", update)
 
 export default route
